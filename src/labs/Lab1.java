c744f857f7973f105bda7e7d5c3ee9904fe840e1
rafaelcaso <aracaso@outlook.com>
2022-10-25 12:55:06 -0400

    initial commit. complete lab1

---
+++ src/labs/Lab1.java
@@ -0,0 +1,32 @@
+package labs;
+
+public class Lab1 {
+	
+	public static void main(String[] args) {
+		
+		boolean boo = true;
+		
+		byte maxByte = 127;
+		
+		short minShort = -32768;
+		
+		int maxInt = 2_000_000_000;
+		
+		long javaFlex = 3_000_000_000L;
+		
+		float pie = 3.14159f;
+		
+		double doubleYum = 3.14159f * 2;
+		
+		System.out.println(boo);
+		System.out.println(maxByte);
+		System.out.println(minShort);
+		System.out.println(maxInt);
+		System.out.println(javaFlex);
+		System.out.println(pie);
+		System.out.println(doubleYum);
+		
+		
+	}
+
+}
