e065a80f3b12544f4196955de70c08744c0d8c13
rafaelcaso <aracaso@outlook.com>
2022-10-25 17:47:21 -0400

    add lab3

---
+++ src/labs/Lab3.java
@@ -0,0 +1,30 @@
+package labs;
+
+import java.util.Arrays;
+
+public class Lab3 {
+
+	public String replaceAllSpaces(String input) {
+		return input.replace(" ", "-");
+	}
+	
+	// this will still return true if sequence of characters is in inputWord and not necessarily an entire word
+	public static boolean containsWord(String inputWord, String searchWord) {
+		String inputWordLowerCase = inputWord.toLowerCase();
+		return inputWordLowerCase.contains(searchWord);
+	}
+	
+	public static String[] splitPhoneNumber(String phoneNumber) {
+		return phoneNumber.split("-");
+		
+	}
+	
+	public static void main(String[] args) {
+		Lab3 lab3 = new Lab3();
+		
+		System.out.println(lab3.replaceAllSpaces("Hey world"));
+		System.out.println(lab3.containsWord("HELLO WOrld", "world"));
+		System.out.println(Arrays.toString(lab3.splitPhoneNumber("555-123-4567")));
+	}
+	
+}
